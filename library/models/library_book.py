e308abdbde79ae36734ae7eaf34507afe27cbf93
D4ynx <daynx.iism@gmail.com>
2026-06-06 18:20:49 +0800

    Add computed field and action_feature method to LibraryBook

--- library/models/library_book.py
+++ library/models/library_book.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import date
 
 class LibraryBook(models.Model):
     _name = 'library.book'
@@ -8,4 +9,24 @@ class LibraryBook(models.Model):
     author = fields.Char(string='Author', required=True)
     isbn = fields.Char(string='ISBN')
     date_published = fields.Date(string='Date Published')
-    active = fields.Boolean(string='Active', default=True)    
+    active = fields.Boolean(string='Active', default=True)
+    is_featured = fields.Boolean(string='Featured', default=False)
+    
+    days_since_published = fields.Integer(
+        string = 'Days Since Published',
+        compute = '_compute_days_since_published'
+        )
+    
+    @api.depends('date_published')
+    def _compute_days_since_published(self):    
+        for record in self:
+            if record.date_published:
+                record.days_since_published = (date.today() - record.date_published).days
+            else:
+                record.days_since_published = 0
+                
+    def action_feature(self):
+        for record in self:
+            record.is_featured = True
+    
+        
